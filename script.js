061871c690853ad368edc1fd22254ea2c2e79f1f
nafis195 <nafisbuet1@gmail.com>
2020-04-14 12:12:46 -0700

    added jquery & buttons

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ Time - 10:06 PM
 
 
 
+var openCaseVal = 0;
+$("#openBtn").append(openCaseVal);
+var closeCaseVal = 0;
+$("#closeBtn").append(closeCaseVal);
+var deleteCaseVal = 0;
+$("#deleteBtn").append(deleteCaseVal);
+
+
+function submitIssue(){
+    const getInputValue = id => document.getElementById(id).value;
+    const issueDescription = getInputValue('description');
+    const issueSeverity = getInputValue('severity');
+    const issueAssignee = getInputValue('assignTo');
+    const issueID = Math.floor(Math.random()*100000000) + '';
+    const issueStatus = 'Open';
+    const issueDate = Date();
+}
